2f62eaad3ecf54c83f68bdffd96408c3f286239d
Michele Coppola <michele.coppola17@outlook.it>
2026-01-24 20:18:19 +0100

    add quit logic

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+VERSION = pygame.version.ver
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
