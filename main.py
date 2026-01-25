078b35a74a510f1ddec816412ff261a19cf02531
Michele Coppola <michele.coppola17@outlook.it>
2026-01-25 17:27:54 +0100

    add player's behaviours

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 VERSION = pygame.version.ver
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
 
 if __name__ == "__main__":
     main()
